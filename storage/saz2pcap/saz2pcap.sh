ac5689f610e9e00877fb4f2f9cb206a04ccea25c
Omri <omriher@gmail.com>
2015-07-30 16:04:53 +0300

    Added fiddler2pcap

---
+++ storage/saz2pcap/saz2pcap.sh
@@ -0,0 +1,8 @@
+for file in  $(ls  $1); 
+	do cd $1;
+	../saz2pcap.py -i $file  -o temp.pcap  --saz;  
+	sudo tcprewrite --infile=temp.pcap --dlt=enet --outfile=temp2.pcap --enet-dmac=00:55:22:AF:C6:37 --enet-smac=00:44:66:FC:29:AF;  
+	sudo tcprewrite --infile=temp2.pcap --outfile=${file%.saz}.pcap  --dstipmap=192.168.2.2:1.1.1.1 --srcipmap=192.168.2.2:1.1.1.1;
+	rm temp.pcap;rm temp2.pcap;
+	cd -;
+done
